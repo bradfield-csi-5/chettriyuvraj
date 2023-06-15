49e5a27bada29d073c485719ee17d63abd88a9d4
Yuvraj Chettri <chettriyuvraj@gmail.com>
2023-06-15 21:24:37 +0530

    2.1: Compute limits of char and int

--- Bradfield CSI/C/Exercises/Chapter2/1.c
+++ Bradfield CSI/C/Exercises/Chapter2/1.c
@@ -7,27 +7,36 @@ using standard headers and by direct computation*/
 
 int main() {
 
-    // Using headers - all signed
+    /* Using headers */
     printf("The max value of char is %d \n", CHAR_MAX);
     printf("The min value of char is %d \n", CHAR_MIN);
-    printf("The max value of short is %hi \n", SHRT_MAX);
-    printf("The min value of short is %hi \n", SHRT_MIN);
-    printf("The max value of unsigned int is %u \n", UINT_MAX); // unsigned
+    printf("The max value of short is %d \n", SHRT_MAX);
+    printf("The min value of short is %d \n", SHRT_MIN);
+    printf("The max value of unsigned int is %u \n", UINT_MAX); /* unsigned */
     printf("The max value of long is %ld \n", LONG_MAX);
     printf("The min value of long is %ld \n", LONG_MIN);
-
-    //floats
-    printf("The max value of float is %.12e \n", FLT_MAX);
+    printf("The max value of float is %.12e \n", FLT_MAX); /* floats */
     printf("The min value of float is %.12e \n", FLT_MIN);
 
-    unsigned int i = 1;
-    unsigned int prevI = 0;
-    while (prevI < i) {
-        prevI = i;
-        i *= 2;
+    /* Computing */
+    char c = 1; /* char */
+    while (c > 0) {
+        c <<= 1;
+        printf("%d \n", c);
+    }
+    printf("The max value of char is %d \n", -c - 1);
+    printf("The min value of char is %d \n", c);
+    printf("The max value of unsigned char is %d \n", -c * 2 - 1);
+
+    int i = 1; /* int */
+    while (i > 0) {
+        i <<= 1;
         printf("%d \n", i);
     }
 
+    printf("The max value of int is %d \n", -i - 1);
+    printf("The min value of int is %d \n", i);
+    printf("The max value of unsigned int is %lu \n", (unsigned long)(-i) * 2 - 1); // Converting to unsigned long to prevent overflow when making i -> -i
 
 
 
