9169157a14e45b424af6f019d8044c71222a6e20
Yuvraj Chettri <chettriyuvraj@gmail.com>
2023-06-16 20:15:47 +0530

    fix: grab n bits (right-sided) starting from pth bit of x - (previously grabbing left-sided n bits)

--- Bradfield CSI/C/Exercises/Chapter2/6.c
+++ Bradfield CSI/C/Exercises/Chapter2/6.c
@@ -3,19 +3,22 @@
 unsigned setbits(unsigned x, int p, int n, unsigned y);
 
 int main() {
-    printf("The result of setbits is %u", setbits(0b101010101, 2, 4, 0b1101101));
+    /* n can be p + 1 at most */
+    printf("The result of setbits is %u", setbits(0b101010101, 4, 2, 0b1101101));
+    // printf("The result of setbits is %u", setbits(122, 5, 3, 147));
+
     return 0;
 }
 
 unsigned setbits(unsigned x, int p, int n, unsigned y) {
     unsigned yRightmostBits = y & ~(~0 << n); /* Grab rightmost n bits from y */
-    unsigned xRightmostBits = (x & ~(~0 << p)); /* Grab rightmost p bits from x */
-    unsigned xLeftmostBits = ( x >> (p + n)); /* Grab leftmost bits from x after removing starting p + n bits */
+    unsigned xRightmostBits = (x & ~(~0 << (p + 1 - n))); /* Grab rightmost (p + 1 - n) bits from x */
+    unsigned xLeftmostBits = ( x >> (p + 1)); /* Grab leftmost bits from x after removing starting p + 1 bits */
 
     unsigned res = xLeftmostBits;
     res <<= n; /* Move bits left by n to make space for n bits from y */
     res |= yRightmostBits; /* Append n rightmost bits of y */
-    res <<= p; /* Move bits left by p to make space for rightmost p bits from x */
+    res <<= (p + 1 - n); /* Move bits left by p to make space for rightmost p + 1 - n bits from x */
     res |= xRightmostBits; /* Append p rightmost bits of x */
 
     return res;
