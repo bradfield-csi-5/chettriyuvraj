0cfca7ffb70cbe9b074ef97a2dd07568da5bb1a1
chettriyuvraj@gmail.com <chettriyuvraj@gmail.com>
2023-08-16 01:49:02 +0530

    1. Prework-3: Further minimize instructions for pangram using bts and sete

--- BradfieldCSI/ComputerArch/Prework3-Assembly/pangram.asm
+++ BradfieldCSI/ComputerArch/Prework3-Assembly/pangram.asm
@@ -1,35 +1,26 @@
 section .text
 global pangram
 pangram:
-	push rbx
+	push rbx; callee saved register
 	xor rbx, rbx; using first 25 bits as a hashmap
-	xor rax, rax; result stored in rax at the end + used to hold each bit of sentence
-
-check:
-     cmp byte[rdi], 0x00
-     je result	
-
 
 forcelower: ; if alphabets, force to lowercase and constrict them to decimal 0-25
 	movzx eax, byte [rdi]
+	cmp eax, 0
+	je result
 	or eax, 32;
         sub eax, 'a'
 
 compute:; create a bitmask and then use it to update hashmap
-	mov r8, 1
-	mov cl, al
-	sal r8, cl
-	or rbx, r8
+	bts rbx, rax
 	inc rdi
-        jmp check; circle back to start of the loop
+        jmp forcelower; circle back to start of the loop
 
 result:; invoked when loop condition fails
 	xor rax, rax; result stored here
 	and rbx, 0x3ffffff; keep only bits 0-25 of hashmap
 	cmp rbx, 0x3ffffff ; comparing our map with that of a pangram to determine if we have a pangram
-	jne done
-	inc rax; input is a pangram
-
+	sete al
 done:
 	pop rbx
 	ret
