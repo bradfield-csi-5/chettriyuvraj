12cc3f6995051c2bfb3d980fec94c79be3b7b02c
chettriyuvraj@gmail.com <chettriyuvraj@gmail.com>
2023-09-18 02:33:05 +0530

    Networks - Prework 1: A little ugly, but complete

--- BradfieldCSI/Networks/Prework-1-Capture-the-Flag/ctf.go
+++ BradfieldCSI/Networks/Prework-1-Capture-the-Flag/ctf.go
@@ -1,7 +1,3 @@
-/*
-
- */
-
 package main
 
 import (
@@ -10,13 +6,14 @@ import (
 	"io"
 	"log"
 	"os"
+	"sort"
 )
 
-const captureFile, pcapHeaderLength = "./net.cap", 24
+const captureFile, pcapHeaderLength, writeFile = "./net.cap", 24, "img.jpg"
 const cols = 4
 
 func main() {
-	file, err := os.Open(captureFile)
+	readFile, err := os.Open(captureFile)
 	if err != nil {
 		log.Fatalf("error in opening file %v", err)
 	}
@@ -26,13 +23,51 @@ func main() {
 		log.Fatalf("Error in parsing pcap header %v", err)
 	}
 
-	packetRecords, err := parsePacketRecords(file)
+	fmt.Println(header)
+
+	packetRecords, err := parsePacketRecords(readFile)
 	if err != nil {
 		log.Fatalf("Error in parsing packet records %v", err)
 	}
 
-	fmt.Println(header)
-	fmt.Println(packetRecords)
+	/* Sort */
+	serverIP := ParseIPv4(ParseEthernet(packetRecords[0].Data).Data).DestIP
+	serverTCPPackets := filterTCPBySourceIP(packetRecords[1:], serverIP) /* Skip SYN */
+	fmt.Println(len(serverTCPPackets))
+	sort.Slice(serverTCPPackets, func(i, j int) bool {
+		return serverTCPPackets[i].SequenceNo < serverTCPPackets[j].SequenceNo
+	})
+
+	/* Remove dups */
+	hashMap := make(map[uint32]bool)
+	uniqueServerTCPPackets := []TCPPacket{}
+	for _, tcpPacket := range serverTCPPackets {
+		_, exists := hashMap[tcpPacket.SequenceNo]
+		if !exists && tcpPacket.Flags != 0x12 { /* Ignore SYN, ACK packet */
+			uniqueServerTCPPackets = append(uniqueServerTCPPackets, tcpPacket)
+			hashMap[tcpPacket.SequenceNo] = true
+		}
+	}
+	serverTCPPackets = uniqueServerTCPPackets
+
+	fmt.Println(len(serverTCPPackets))
+	writeFile, err := os.OpenFile(writeFile, os.O_RDWR|os.O_CREATE, 0777)
+	if err != nil {
+		log.Fatalf("error in opening file to write to %v", err)
+	}
+
+	/* Ignore Title and Headers when HTTP response starts */
+	httpResponseStartPacket := ParseHTTPRequest(serverTCPPackets[0].Data)
+	serverTCPPackets[0].Data = httpResponseStartPacket.Body
+
+	for _, tcpPacket := range serverTCPPackets {
+		if len(tcpPacket.Data) > 0 {
+			_, err := writeFile.Write(tcpPacket.Data)
+			if err != nil {
+				log.Fatalf("error in opening file to write to %v", err)
+			}
+		}
+	}
 }
 
 func parsePcapHeader(file *os.File) (header []byte, err error) {
@@ -109,12 +144,10 @@ func parsePacketRecords(file *os.File) (packetRecords []PacketRecord, err error)
 }
 
 func printPacketRecords(packetRecords []PacketRecord) {
-	fmt.Printf("\nTimestamp\tTimestamp2\tCaptured Length\tOriginal Length")
-	for _, packetRecord := range packetRecords {
-		fmt.Printf("\n%d\t%d\t%d\t%d", packetRecord.Timestamp, packetRecord.Timestamp2, packetRecord.CapturedLen, packetRecord.OriginalLen)
+	fmt.Printf("\nIndex\tTimestamp\tTimestamp2\tCaptured Length\tOriginal Length")
+	for i, packetRecord := range packetRecords {
+		fmt.Printf("\n%d\t%d\t%d\t%d\t%d", i, packetRecord.Timestamp, packetRecord.Timestamp2, packetRecord.CapturedLen, packetRecord.OriginalLen)
 	}
-	fmt.Println("Packet len")
-	fmt.Println(len(packetRecords))
 }
 
 /**
@@ -122,7 +155,6 @@ func printPacketRecords(packetRecords []PacketRecord) {
  *
  * @param byte array containing pcap Packet Record payload
  * @return EthernetFrame
- * @return error if found
  *
  * Grabs fields which are there for sure first
  * We consider that TPID(EtherType) is 4 bytes if VLAN else 2 bytes
@@ -131,7 +163,7 @@ func printPacketRecords(packetRecords []PacketRecord) {
  * Note: Errors eg. invalid lengths not considered for now
  **/
 
-func ParseEthernetFrame(data []byte) EthernetFrame {
+func ParseEthernet(data []byte) EthernetFrame {
 	frame := EthernetFrame{MACdest: data[0:6], MACsource: data[6:12], TPID: data[12:14], TPIDExtended: nil, Data: nil}
 	if frame.TPID[0] == 0x81 {
 		frame.TPIDExtended = data[14:16]
@@ -140,4 +172,112 @@ func ParseEthernetFrame(data []byte) EthernetFrame {
 		frame.Data = data[14:]
 	}
 	return frame
+}
+
+/**
+ * Parses IPv4 packets from the data extracted from Ethernet frames
+ *
+ * @param byte array containing Ethernet payload
+ * @return IPv4 packet
+ *
+ * Grabs all fields in order
+ * Not considering options field / errors for now
+ **/
+
+func ParseIPv4(data []byte) IPv4Packet {
+	packet := IPv4Packet{}
+	packet.Version = data[0] & 0xF0 >> 4                                /* bits 0-3 */
+	packet.IHL = data[0] & 0x0F                                         /* bits 4-7 */
+	packet.DSCP = (data[1] & 0xFC) >> 2                                 /* bits 8-13 */
+	packet.ECN = data[1] & 0x03                                         /* bits 14-15 */
+	packet.TotalLen = binary.BigEndian.Uint16(data[2:4])                /* bits 16-31 bytes 2,3 */ // (uint16(data[2]) & 0xFF00) & (0x00FF & uint16(data[3]))
+	packet.Id = binary.BigEndian.Uint16(data[4:6])                      /* bytes 4,5 */
+	packet.Flags = (data[6] & 0xE0) >> 5                                /* byte 6 bits 0-3 */
+	packet.FragmentOffset = binary.BigEndian.Uint16(data[6:8]) & 0x1FFF /* byte 6, 7, last 13 bits */
+	packet.TTL = data[8]                                                /* byte 8 */
+	packet.Protocol = data[9]                                           /* byte 9 */
+	packet.HeaderChecksum = binary.BigEndian.Uint16(data[10:12])        /* bytes 10,11 */
+	packet.SourceIP = binary.BigEndian.Uint32(data[12:16])              /* bytes 12-15 */
+	packet.DestIP = binary.BigEndian.Uint32(data[16:20])                /* bytes 16-19 */
+	// packet.Data = data[packet.IHL*4 : packet.TotalLen]
+	packet.Data = data[packet.IHL*4:]
+	return packet
+}
+
+/**
+ * Parses TCP packets from the data extracted from IPv4
+ *
+ * @param byte array containing IPv4
+ * @return TCPPacket
+ *
+ * Grabs all fields in order
+ * Not considering options field / errors for now
+ **/
+
+func ParseTCP(data []byte) TCPPacket {
+	packet := TCPPacket{}
+	packet.SourcePort = binary.BigEndian.Uint16(data[0:2])
+	packet.DestPort = binary.BigEndian.Uint16(data[2:4])
+	packet.SequenceNo = binary.BigEndian.Uint32(data[4:8])
+	packet.AckNo = binary.BigEndian.Uint32(data[8:12])
+	packet.DataOffset = data[12] & 0xFF >> 4
+	packet.Reserved = data[12] & 0x0F
+	packet.Flags = data[13] & 0xFF
+	packet.WindowSize = binary.BigEndian.Uint16(data[14:16])
+	packet.Checksum = binary.BigEndian.Uint16(data[16:18])
+	packet.UrgentPtr = binary.BigEndian.Uint16(data[18:20])
+	packet.Data = data[packet.DataOffset*4:]
+	return packet
+}
+
+func ParseHTTPRequest(data []byte) HTTPRequest {
+	request := HTTPRequest{}
+	title := []byte{}
+	headers := []byte{}
+	body := []byte{}
+	offset := 0
+	newLine := byte(0xA)
+	carriageReturn := byte(0xD)
+	/* Grab title line */
+	for offset = 0; offset < len(data) && (data[offset] != carriageReturn || data[offset+1] != newLine); offset++ {
+		title = append(title, data[offset])
+	}
+
+	offset += 2
+	/* Grab headers */
+	for ; offset < len(data) && (data[offset] != carriageReturn || data[offset+1] != newLine || data[offset+2] != carriageReturn || data[offset+3] != newLine); offset++ {
+		headers = append(headers, data[offset])
+	}
+
+	offset += 4
+	/* Grab request body */
+	for ; offset < len(data) && (data[offset] != carriageReturn || data[offset+1] != newLine); offset++ {
+		body = append(body, data[offset])
+	}
+
+	request.Title = title
+	request.Headers = headers
+	request.Body = body
+
+	return request
+}
+
+/**
+ * Takes Packet Records, returns TCP Packets filtered by Source IP
+ *
+ * @param slice of packet records
+ * @param source ip
+ * @return slice of TCP Packets
+ **/
+
+func filterTCPBySourceIP(packetRecords []PacketRecord, sourceIP uint32) []TCPPacket {
+	filteredTCPPackets := []TCPPacket{}
+	for _, packetRecord := range packetRecords {
+		ipPacket := ParseIPv4(ParseEthernet(packetRecord.Data).Data)
+		if ipPacket.SourceIP == sourceIP {
+			tcpPacket := ParseTCP(ipPacket.Data)
+			filteredTCPPackets = append(filteredTCPPackets, tcpPacket)
+		}
+	}
+	return filteredTCPPackets
 }
