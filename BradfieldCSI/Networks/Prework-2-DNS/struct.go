056532bd34961ca0de7f3fee72c8343ec08f7e82
chettriyuvraj@gmail.com <chettriyuvraj@gmail.com>
2023-09-21 06:04:08 +0530

    Networking - Prework - 2 - DNS
    1. refactor: remove redundant packet parsing functionality and structs

--- BradfieldCSI/Networks/Prework-2-DNS/struct.go
+++ BradfieldCSI/Networks/Prework-2-DNS/struct.go
@@ -29,38 +29,3 @@ type DNSAnswer struct {
 	RDLength uint16
 	RData    []byte
 }
-
-type EthernetFrame struct {
-	MACdest      []byte
-	MACsource    []byte
-	TPID         []byte
-	TPIDExtended []byte
-	Data         []byte
-	// CRC          []byte /* Not considering CRC for now */
-}
-
-type IPv4Packet struct {
-	Version        uint8
-	IHL            uint8
-	DSCP           uint8
-	ECN            uint8
-	TotalLen       uint16
-	Id             uint16
-	Flags          uint8
-	FragmentOffset uint16
-	TTL            uint8
-	Protocol       uint8
-	HeaderChecksum uint16
-	SourceIP       uint32
-	DestIP         uint32
-	/* Options */
-	Data []byte
-}
-
-type UDPPacket struct {
-	SourcePort uint16
-	DestPort   uint16
-	Length     uint16
-	Checksum   uint16
-	Data       []byte
-}
