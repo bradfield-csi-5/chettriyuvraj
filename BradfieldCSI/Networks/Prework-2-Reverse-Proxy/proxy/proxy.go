097956b0c6788d1716cb84c2ba17995ce6276b9c
chettriyuvraj@gmail.com <chettriyuvraj@gmail.com>
2023-09-25 05:09:26 +0530

    1. feat: parallel proxy server with forwarding to test server

--- BradfieldCSI/Networks/Prework-2-Reverse-Proxy/proxy/proxy.go
+++ BradfieldCSI/Networks/Prework-2-Reverse-Proxy/proxy/proxy.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"sync"
 
 	"golang.org/x/sys/unix"
 )
@@ -10,90 +11,134 @@ import (
 const PROXYPORTCLIENT, SERVERPORT = 7542, 9000
 const NEWLINE, CARRIAGERETURN = 0x0a, 0x0d
 
+// const CACHEPATH = "/cache"
+
+// var cacheconf CacheConf = CacheConf{
+// 	ProxyCachePath: CACHEPATH,
+// 	Server: []Location{
+// 		{Path: "/", ProxyPass: "localhost:7542"},
+// 		{Path: "/", ProxyPass: "localhost:7543"},
+// 	},
+// }
+
 var LOOPBACK [4]byte = [4]byte{0x7F, 0x00, 0x00, 0x01}
+var clientSocketChannel chan int = make(chan int, 3)
+var errorChannel chan error = make(chan error, 30)
+var wg sync.WaitGroup
 
 func main() {
-	clientSocket, _, err := ConnectToClient()
-	if err != nil {
-		log.Fatalf("error connecting to client %v", err)
-	}
 
-	err = ForwardClientToServer(clientSocket)
-	if err != nil {
-		log.Fatalf("error forwarding to server %v", err)
-	}
+	wg.Add(3)
+	go handleErrors()
+	go ForwardClientToServer()
+	go ConnectToClient()
+	wg.Wait()
+
 }
 
-func ConnectToClient() (int, unix.Sockaddr, error) {
+func ConnectToClient() (int, int, error) {
+
+	defer wg.Done()
+
 	fmt.Println("Creating socket to connect to client...")
 	socket, err := unix.Socket(unix.AF_INET, unix.SOCK_STREAM, 0)
 	if err != nil {
-		return -1, nil, fmt.Errorf("error creating client socket %v", err)
+		log.Fatalf("error creating client socket %v", err)
 	}
 
+	fmt.Println("Binding socket to connect to client...")
 	sockaddr := &unix.SockaddrInet4{Port: PROXYPORTCLIENT, Addr: LOOPBACK}
 	err = unix.Bind(socket, sockaddr)
 	if err != nil {
-		return -1, nil, fmt.Errorf("error binding socket %v", err)
+		log.Fatalf("error binding socket %v", err)
 	}
 
-	fmt.Println("Listening...")
-	err = unix.Listen(socket, 1)
-	if err != nil {
-		return -1, nil, fmt.Errorf("error while listening %v", err)
-	}
-
-	nfd, nsa, err := unix.Accept(socket)
-	if err != nil {
-		return -1, nil, fmt.Errorf("error while accepting %v", err)
-	}
-	fmt.Println("Accepted...")
-
-	return nfd, nsa, err
-}
-
-func ForwardClientToServer(clientSocket int) error {
-	clientBuffer := make([]byte, 4096)
+	defer unix.Close(socket)
 
 	for {
+		fmt.Println("Listening...")
+		err = unix.Listen(socket, 3)
+		if err != nil {
+			log.Fatalf("error while listening %v", err)
+		}
+
+		nfd, _, err := unix.Accept(socket)
+		if err != nil {
+			errorChannel <- fmt.Errorf("error while accepting %v", err)
+			continue
+		}
+
+		fmt.Println("Accepted...")
+		clientSocketChannel <- nfd
+	}
+
+}
+
+func ForwardClientToServer() {
+	clientBuffer := make([]byte, 4096)
+	serverBuffer := make([]byte, 4096)
+
+	for rwClientSocket := range clientSocketChannel {
+
 		fmt.Println("Creating socket to connect to server...")
 		serverSocket, err := unix.Socket(unix.AF_INET, unix.SOCK_STREAM, 0)
 		if err != nil {
-			return fmt.Errorf("error creating server socket %v", err)
-		}
-
-		sockaddr := &unix.SockaddrInet4{Addr: LOOPBACK}
-		err = unix.Bind(serverSocket, sockaddr)
-		if err != nil {
-			return fmt.Errorf("error binding socket %v", err)
+			errorChannel <- fmt.Errorf("error creating server socket %v", err)
+			continue
 		}
 
 		fmt.Println("Receiving from client...")
-		n, _, err := unix.Recvfrom(clientSocket, clientBuffer, 0)
+		_, _, err = unix.Recvfrom(rwClientSocket, clientBuffer, 0)
 		if err != nil {
-			log.Fatalf("error while receiving from client %v", err)
-		}
-		if n == 0 {
-			break
+			errorChannel <- fmt.Errorf("error while receiving from client %v", err)
+			continue
 		}
 
 		fmt.Println("Connecting to server...")
 		err = unix.Connect(serverSocket, &unix.SockaddrInet4{Port: SERVERPORT, Addr: LOOPBACK})
 		if err != nil {
-			log.Fatalf("error connecting to server %v", err)
+			errorChannel <- fmt.Errorf("error connecting to server %v", err)
+			continue
 		}
 
-		fmt.Printf("\nReceived %q, passing to server...", string(clientBuffer))
+		// fmt.Printf("\nReceived %q, passing to server...", string(clientBuffer))
+		fmt.Println("Received, passing to server...")
 		err = unix.Send(serverSocket, clientBuffer, 0)
 		if err != nil {
-			log.Fatalf("error while sending to server %v", err)
+			errorChannel <- fmt.Errorf("error while sending to server %v", err)
+			continue
 		}
 
-		fmt.Println("\nSuccessfully passed to server, closing connection...")
+		fmt.Printf("\nGetting response from server")
+		_, _, err = unix.Recvfrom(serverSocket, serverBuffer, 0)
+		if err != nil {
+			errorChannel <- fmt.Errorf("error while receiving from client %v", err)
+			continue
+		}
+
+		fmt.Println("\nReceived response from server, closing connection...")
 		err = unix.Close(serverSocket)
 		if err != nil {
-			log.Fatalf("error while closing server socket %v", err)
+			errorChannel <- fmt.Errorf("error while closing server socket %v", err)
 		}
+
+		fmt.Println("Passing server response to client...")
+		err = unix.Send(rwClientSocket, serverBuffer, 0)
+		if err != nil {
+			errorChannel <- fmt.Errorf("error while sending server response to client %v", err)
+		}
+
+		err = unix.Close(rwClientSocket)
+		if err != nil {
+			errorChannel <- fmt.Errorf("error while closing client rw socket %v", err)
+		}
+
 	}
-	return nil
+}
+
+func handleErrors() {
+	defer wg.Done()
+	for err := range errorChannel {
+		fmt.Printf("non-terminating error %q", err)
+	}
 }
