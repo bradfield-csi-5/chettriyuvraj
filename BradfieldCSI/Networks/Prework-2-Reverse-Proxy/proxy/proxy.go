93b6193c0b1b374fe2a9059b1be79581b7be3da9
chettriyuvraj@gmail.com <chettriyuvraj@gmail.com>
2023-09-25 20:48:26 +0530

    1. feat: parallelize reverse proxy 2. refactor: separate cache check logic, defer socket close() calls 3. fix: add missing wg.done calls

--- BradfieldCSI/Networks/Prework-2-Reverse-Proxy/proxy/proxy.go
+++ BradfieldCSI/Networks/Prework-2-Reverse-Proxy/proxy/proxy.go
@@ -18,14 +18,14 @@ import (
 
 const PROXYPORTCLIENT, SERVERPORT = 7543, 9000
 const NEWLINE, CARRIAGERETURN = 0x0a, 0x0d
-
 const CACHEPATH = "./cache"
+const MAXLISTENQUEUE = 3
 
 var cacheconf CacheConf = CacheConf{
 	ProxyCachePath: CACHEPATH,
 	Server: []Location{
 		{Path: "/proxy", ProxyPath: [4]byte{0x7F, 0x00, 0x00, 0x01}, ProxyPort: 9000},
-		{Path: "/local", ProxyPort: -1}, // store in order of least to most specific
+		{Path: "/local", ProxyPort: -1}, // always store in order of least to most specific
 	},
 }
 
@@ -47,6 +47,8 @@ func main() {
 func ConnectToClient() (int, int, error) {
 
 	defer wg.Done()
+	defer close(clientSocketChannel)
+	defer close(errorChannel)
 
 	fmt.Println("Creating socket to connect to client...")
 	socket, err := unix.Socket(unix.AF_INET, unix.SOCK_STREAM, 0)
@@ -65,7 +67,7 @@ func ConnectToClient() (int, int, error) {
 
 	for {
 		fmt.Println("Listening...")
-		err = unix.Listen(socket, 3)
+		err = unix.Listen(socket, MAXLISTENQUEUE)
 		if err != nil {
 			log.Fatalf("error while listening %v", err)
 		}
@@ -83,172 +85,141 @@ func ConnectToClient() (int, int, error) {
 }
 
 func ForwardClientToServer() {
+
+	defer wg.Done()
+
 	clientBuffer := make([]byte, 4096)
 	serverBuffer := make([]byte, 4096)
 
 	for rwClientSocket := range clientSocketChannel {
 
-		var matchLocation Location = Location{Path: "", ProxyPort: -1}
-
-		fmt.Println("Creating socket to connect to server...")
-		serverSocket, err := unix.Socket(unix.AF_INET, unix.SOCK_STREAM, 0)
-		if err != nil {
-			errorChannel <- fmt.Errorf("error creating server socket %v", err)
-			continue
-		}
-
-		fmt.Println("Receiving from client...")
-		_, _, err = unix.Recvfrom(rwClientSocket, clientBuffer, 0)
-		if err != nil {
-			errorChannel <- fmt.Errorf("error while receiving from client %v", err)
-			continue
-		}
-
-		req, err := http.ReadRequest(bufio.NewReader(bytes.NewReader(clientBuffer)))
-		for _, location := range cacheconf.Server {
-			if strings.Contains(req.URL.Path, location.Path) { // location.Path stored in order of least to most specific
-				fmt.Println("match")
-				fmt.Println(location)
-				matchLocation = location
-			}
-		}
-
-		fmt.Println(matchLocation)
-
-		if matchLocation.Path == "" { // return 404
-			errorChannel <- fmt.Errorf("location not found on server %v", err)
-			continue
-		}
-
-		if matchLocation.ProxyPort == -1 { // no proxy
-			fmt.Println("No proxy..")
-			response := sampleResponse("no proxy")
-			tempBuffer := bytes.NewBuffer(serverBuffer)
-			err = response.Write(tempBuffer)
-			if err != nil {
-				errorChannel <- fmt.Errorf("error while writing non proxy response to buffer %v", err)
-				continue
-			}
-
-			err = unix.Send(rwClientSocket, tempBuffer.Bytes(), 0)
-			if err != nil {
-				errorChannel <- fmt.Errorf("error while sending non proxy server response to client %v", err)
-			}
-
-			err = unix.Close(rwClientSocket)
+		defer func() {
+			err := unix.Close(rwClientSocket)
 			if err != nil {
 				errorChannel <- fmt.Errorf("error while closing client non proxy rw socket %v", err)
 			}
-			continue
-		}
+		}()
 
-		fmt.Println("Checking cache...")
-		basePath := path.Base(req.URL.Path)
-		filePath := CACHEPATH + "/" + basePath
-		fmt.Println(filePath)
-		f, err := os.OpenFile(filePath, os.O_RDONLY, 0777)
-		if err == nil {
-			fmt.Println("Serving cache...")
-			for {
-				_, err = f.Read(serverBuffer)
-				if err == io.EOF {
-					fmt.Println("Done writing cache to buffer...")
-					break
-				}
-				if err != nil {
-					errorChannel <- fmt.Errorf("error while writing cache to buffer %v", err)
-					break
+		go func() {
+			var matchLocation Location = Location{Path: "", ProxyPort: -1}
+
+			fmt.Println("Creating socket to connect to server...")
+			serverSocket, err := unix.Socket(unix.AF_INET, unix.SOCK_STREAM, 0)
+			if err != nil {
+				errorChannel <- fmt.Errorf("error creating server socket %v", err)
+				return
+			}
+
+			fmt.Println("Receiving from client...")
+			_, _, err = unix.Recvfrom(rwClientSocket, clientBuffer, 0)
+			if err != nil {
+				errorChannel <- fmt.Errorf("error while receiving from client %v", err)
+				return
+			}
+
+			req, err := http.ReadRequest(bufio.NewReader(bytes.NewReader(clientBuffer)))
+			for _, location := range cacheconf.Server {
+				if strings.Contains(req.URL.Path, location.Path) { // location.Path stored in order of least to most specific
+					// fmt.Println("match")
+					// fmt.Println(location)
+					matchLocation = location
 				}
 			}
-			if err == io.EOF || err == nil {
 
-				response := sampleResponse(string(serverBuffer))
-				tempBuffer := bytes.NewBuffer([]byte{})
+			// fmt.Println(matchLocation)
+
+			if matchLocation.Path == "" { // return 404
+				errorChannel <- fmt.Errorf("location not found on server %v", err)
+				return
+			}
+
+			if matchLocation.ProxyPort == -1 { // no proxy
+				fmt.Println("No proxy..")
+				response := sampleResponse("no proxy")
+				tempBuffer := bytes.NewBuffer(serverBuffer)
 				err = response.Write(tempBuffer)
+				if err != nil {
+					errorChannel <- fmt.Errorf("error while writing non proxy response to buffer %v", err)
+					return
+				}
 
 				err = unix.Send(rwClientSocket, tempBuffer.Bytes(), 0)
 				if err != nil {
+					errorChannel <- fmt.Errorf("error while sending non proxy server response to client %v", err)
+				}
+
+				return
+			}
+
+			cache := checkCacheAndServe(req)
+			if cache != nil {
+				fmt.Println("Serving cache...")
+				err = unix.Send(rwClientSocket, cache, 0)
+				if err != nil {
 					errorChannel <- fmt.Errorf("error while sending  proxy server response to client %v", err)
 				}
-
-				err = unix.Close(rwClientSocket)
-				if err != nil {
-					errorChannel <- fmt.Errorf("error while closing client  proxy rw socket %v", err)
-				}
+				return
 			}
-			continue
-		}
 
-		fmt.Println("File not in cache")
-		fmt.Println("Connecting to server...")
-		err = unix.Connect(serverSocket, &unix.SockaddrInet4{Port: matchLocation.ProxyPort, Addr: matchLocation.ProxyPath})
-		if err != nil {
-			errorChannel <- fmt.Errorf("error connecting to server %v", err)
-			continue
-		}
+			fmt.Println("Connecting to server...")
+			err = unix.Connect(serverSocket, &unix.SockaddrInet4{Port: matchLocation.ProxyPort, Addr: matchLocation.ProxyPath})
+			if err != nil {
+				errorChannel <- fmt.Errorf("error connecting to server %v", err)
+				return
+			}
 
-		// fmt.Printf("\nReceived %q, passing to server...", string(clientBuffer))
-		fmt.Println("Received, passing to server...")
-		err = unix.Send(serverSocket, clientBuffer, 0)
-		if err != nil {
-			errorChannel <- fmt.Errorf("error while sending to server %v", err)
-			continue
-		}
+			// fmt.Printf("\nReceived %q, passing to server...", string(clientBuffer))
+			fmt.Println("Received, passing to server...")
+			err = unix.Send(serverSocket, clientBuffer, 0)
+			if err != nil {
+				errorChannel <- fmt.Errorf("error while sending to server %v", err)
+				return
+			}
 
-		fmt.Printf("\nGetting response from server")
-		_, _, err = unix.Recvfrom(serverSocket, serverBuffer, 0)
-		if err != nil {
-			errorChannel <- fmt.Errorf("error while receiving from client %v", err)
-			continue
-		}
+			fmt.Printf("\nGetting response from server")
+			_, _, err = unix.Recvfrom(serverSocket, serverBuffer, 0)
+			if err != nil {
+				errorChannel <- fmt.Errorf("error while receiving from client %v", err)
+				return
+			}
 
-		fmt.Println("\nReceived response from server, closing connection...")
-		err = unix.Close(serverSocket)
-		if err != nil {
-			errorChannel <- fmt.Errorf("error while closing server socket %v", err)
-		}
+			resp, err := http.ReadResponse(bufio.NewReader(bytes.NewReader(serverBuffer)), req)
+			respData := make([]byte, 4096)
+			cacheFile, err := os.OpenFile(CACHEPATH+"/"+path.Base(req.URL.Path), os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0777)
+			if err != nil {
+				errorChannel <- fmt.Errorf("error while opening cache file to write to %v", err)
+			}
+			for {
+				_, err := resp.Body.Read(respData)
+				fmt.Println(string(respData))
+				if err == io.EOF {
+					_, err = cacheFile.Write(respData)
+					if err != nil {
+						errorChannel <- fmt.Errorf("error while writing cache file %v", err)
+					}
+					break
+				}
+				if err != nil {
+					errorChannel <- fmt.Errorf("error while opening cache file to write to %v", err)
+				}
 
-		resp, err := http.ReadResponse(bufio.NewReader(bytes.NewReader(serverBuffer)), req)
-		respData := make([]byte, 4096)
-		cacheFile, err := os.OpenFile(filePath, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0777)
-		if err != nil {
-			errorChannel <- fmt.Errorf("error while opening cache file to write to %v", err)
-		}
-		for {
-			_, err := resp.Body.Read(respData)
-			fmt.Println(string(respData))
-			if err == io.EOF {
 				_, err = cacheFile.Write(respData)
 				if err != nil {
 					errorChannel <- fmt.Errorf("error while writing cache file %v", err)
 				}
-				break
 			}
+			err = cacheFile.Close()
 			if err != nil {
-				errorChannel <- fmt.Errorf("error while opening cache file to write to %v", err)
+				errorChannel <- fmt.Errorf("error while closing cache file to write to %v", err)
 			}
 
-			_, err = cacheFile.Write(respData)
+			fmt.Println("Passing server response to client...")
+			err = unix.Send(rwClientSocket, serverBuffer, 0)
 			if err != nil {
-				errorChannel <- fmt.Errorf("error while writing cache file %v", err)
+				errorChannel <- fmt.Errorf("error while sending server response to client %v", err)
 			}
-		}
-		err = cacheFile.Close()
-		if err != nil {
-			errorChannel <- fmt.Errorf("error while closing cache file to write to %v", err)
-		}
 
-		fmt.Println("Passing server response to client...")
-		err = unix.Send(rwClientSocket, serverBuffer, 0)
-		if err != nil {
-			errorChannel <- fmt.Errorf("error while sending server response to client %v", err)
-		}
-
-		err = unix.Close(rwClientSocket)
-		if err != nil {
-			errorChannel <- fmt.Errorf("error while closing client rw socket %v", err)
-		}
-
+		}()
 	}
 }
 
@@ -259,7 +230,47 @@ func handleErrors() {
 	}
 }
 
-func sampleResponse(body string) *http.Response { // Giving error when sending to curl
+func checkCacheAndServe(req *http.Request) []byte {
+	fmt.Println("Checking cache...")
+
+	serverBuffer := make([]byte, 4096)
+	basePath := path.Base(req.URL.Path)
+	filePath := CACHEPATH + "/" + basePath
+	f, err := os.OpenFile(filePath, os.O_RDONLY, 0777)
+
+	if err != nil {
+		if os.IsNotExist(err) {
+			fmt.Println("No cache exists")
+			return nil
+		}
+		errorChannel <- fmt.Errorf("error while opening cache file %v", err)
+		return nil
+	}
+
+	for {
+		_, err = f.Read(serverBuffer)
+		if err == io.EOF {
+			fmt.Println("Done writing cache to buffer...")
+			break
+		}
+		if err != nil {
+			errorChannel <- fmt.Errorf("error while writing cache to buffer %v", err)
+			return nil
+		}
+	}
+
+	response := sampleResponse(string(serverBuffer))
+	tempBuffer := bytes.NewBuffer([]byte{})
+	err = response.Write(tempBuffer)
+	if err != nil {
+		errorChannel <- fmt.Errorf("error while writing cache to temp buffer %v", err)
+		return nil
+	}
+	return tempBuffer.Bytes()
+
+}
+
+func sampleResponse(body string) *http.Response {
 	t := &http.Response{
 		StatusCode:    http.StatusOK,
 		Proto:         "HTTP/1.1",
@@ -276,8 +287,3 @@ func sampleResponse(body string) *http.Response { // Giving error when sending t
 	t.Header.Set("Date", "Sun, 24 Sep 2023 14:00:00 GMT")    // Set the Date header
 	return t
 }
-
-// whenever a request comes in:
-// match it with the locations most specific
-// check if the file exists in cachePath directory, if yes return it back
-// otherwise route to server, save the data when it returns from the server to cache folder
