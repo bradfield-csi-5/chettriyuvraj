77f3c9ba6b0e35925820a263a0b2cf69531d1a7f
chettriyuvraj@gmail.com <chettriyuvraj@gmail.com>
2023-10-23 07:38:34 +0530

    feat: implement assign statements conversion to assembly

--- BradfieldCSI/Compilers/compilers-overview-prework/03-compiler/compile.go
+++ BradfieldCSI/Compilers/compilers-overview-prework/03-compiler/compile.go
@@ -15,9 +15,18 @@ import (
 // Recall from the README that the input parameters `x` and `y` should
 // be read from memory addresses `1` and `2`, and the return value
 // should be written to memory address `0`.
+
+/* Map variable name to its location in memory + next location in line to be mapped */
+var varToMem map[string]int = make(map[string]int)
+var nextMemoryLocation int = 3
+
 func compile(node *ast.FuncDecl) (string, error) {
 	var b strings.Builder
 	assemblyList := make([]strings.Builder, 0)
+
+	/* Assumptions */
+	varToMem["x"] = 1
+	varToMem["y"] = 2
 
 	/* Iterate over Body.List of FuncDecl */
 	for _, n := range node.Body.List {
@@ -43,6 +52,10 @@ func evalNode(node ast.Stmt) ([]strings.Builder, error) {
 	switch n := node.(type) {
 	case *ast.ReturnStmt:
 		res, err = evalReturn(n)
+	case *ast.AssignStmt:
+		res, err = evalAssign(n)
+		/* 	case *ast.IfStmt:
+		res, err = evalIfElse(n) */
 	}
 
 	if err != nil {
@@ -70,6 +83,65 @@ func evalReturn(node *ast.ReturnStmt) ([]strings.Builder, error) {
 	return sb, nil
 }
 
+/* General Format of AssignStmt to assembly:
+{assembly for evaluated expression which puts it on top of the stack}
+pop <next memory location>
+*/
+
+func evalAssign(node *ast.AssignStmt) ([]strings.Builder, error) {
+	/* Since its the assign stmt, we are expecting an identifier on the LHS */
+	var b strings.Builder
+	sb, err := evalExpr(node.Rhs[0])
+	if err != nil {
+		return nil, err
+	}
+
+	/* Expecting Ident on LHS */
+	ident, ok := node.Lhs[0].(*ast.Ident)
+	if ok != true {
+		return nil, fmt.Errorf("invalid RHS of assignment")
+	}
+
+	/* Take assignment var, record it in map for later reference, also generate assembly to pop it to memory location*/
+	varName := ident.Name
+	varToMem[varName] = nextMemoryLocation
+	fmt.Fprintf(&b, "pop %d\n", nextMemoryLocation)
+	nextMemoryLocation += 1
+	sb = append(sb, b)
+
+	return sb, nil
+}
+
+// func evalIfElse(node *ast.IfStmt) ([]strings.Builder, error) {
+// 	sb := []strings.Builder{}
+// 	var l1, l2 strings.Builder
+// 	l1.WriteString("l1:\n")
+// 	l2.WriteString("l2:\n")
+
+// 	/* Determine which if label to be executed depending on result of if body */
+// 	sbcond, err := evalExpr(node.Cond)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	/* We will assign all conditions a label */
+// 	sbif, err := evalNode(node.Body)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	sbelse, err := evalNode(node.Else)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	sb = append(sb, sbcond...)
+// 	sb = append(sb, sbif...)
+// 	sb = append(sb, sbelse...)
+
+// 	return sb, nil
+// }
+
 func evalExpr(node ast.Expr) ([]strings.Builder, error) {
 	var res []strings.Builder
 	var err error
@@ -85,6 +157,18 @@ func evalExpr(node ast.Expr) ([]strings.Builder, error) {
 		}
 
 		fmt.Fprintf(&b, "pushi %d\n", byte(tempRes))
+		res = append(res, b)
+
+	case *ast.Ident:
+		var b strings.Builder
+
+		memoryLocation, exists := varToMem[n.Name]
+		if exists != true {
+			err = fmt.Errorf("variable does not exist")
+			break
+		}
+
+		fmt.Fprintf(&b, "push %d\n", memoryLocation)
 		res = append(res, b)
 
 	case *ast.BinaryExpr:
@@ -145,6 +229,8 @@ func getOpAsStringBuilder(op token.Token) strings.Builder {
 		res.WriteString("mul\n")
 	case token.QUO:
 		res.WriteString("div\n")
+	case token.EQL:
+		res.WriteString("eq\njeqz l2")
 	}
 	return res
 }
