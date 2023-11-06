c8a24d59dc16bf32bd9d77a5e1ba81457fd64352
chettriyuvraj@gmail.com <chettriyuvraj@gmail.com>
2023-11-06 23:00:07 +0530

    1. feat: splitting up array row wise between threads

--- BradfieldCSI/Compilers/concurrency-1-prework/matrix/matrix-multiply.c
+++ BradfieldCSI/Compilers/concurrency-1-prework/matrix/matrix-multiply.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-#define MAXTHREADS 100000
+#define MAXTHREADS 32
 void *thread(void *arg);
 struct matrix_mult {
   double **A;
   double **B;
   double **C;
-  int i, j, k;
+  int a_cols, b_cols, a_start_row, a_end_row;
 };
 
 /*
@@ -37,29 +37,40 @@ void matrix_multiply(double **C, double **A, double **B, int a_rows, int a_cols,
 }
 
 void parallel_matrix_multiply(double **C, double **A, double **B, int a_rows, int a_cols, int b_cols) {
-  pthread_t thread_id[MAXTHREADS];
+  pthread_t thread_id[MAXTHREADS + 1];
   struct matrix_mult * m;
-  int thread_count = 0;
+  int start_row=0, end_row=0, row_per_thread = a_rows/MAXTHREADS;
+  int extra_thread = 0;
 
-  /* One thread for each computation */
-  for (int i = 0; i < a_rows; i++) {
-    for (int j = 0; j < b_cols; j++) {
-      C[i][j] = 0;
-      for (int k = 0; k < a_cols; k++) {
-        m = malloc(sizeof(struct matrix_mult));
-        // m =  {A, B, C, i, j, k};
-        (*m).A = A;
-        (*m).B = B;
-        (*m).C = C;
-        (*m).i = i;
-        (*m).j = j;
-        (*m).k = k;
-        pthread_create(&thread_id[thread_count++], NULL, thread, m);
-      }
-    }
+  /* Split rows up by thread */
+  for (int i = 0; i < MAXTHREADS; i++) {
+    end_row += row_per_thread;
+    m = malloc(sizeof(struct matrix_mult));
+    (*m).A = A;
+    (*m).B = B;
+    (*m).C = C;
+    (*m).a_cols = a_cols;
+    (*m).b_cols = b_cols;
+    (*m).a_start_row = start_row;
+    (*m).a_end_row = end_row;
+    pthread_create(&thread_id[i], NULL, thread, m);
+    start_row = end_row;
   }
 
-  for (int i = 0; i<thread_count; i++) {
+  /* Leftover rows */
+  if (end_row != a_rows) {
+    extra_thread = 1;
+    m = malloc(sizeof(struct matrix_mult));
+    (*m).A = A;
+    (*m).B = B;
+    (*m).C = C;
+    (*m).a_cols = a_cols;
+    (*m).a_start_row = start_row;
+    (*m).a_end_row = a_cols;
+    pthread_create(&thread_id[MAXTHREADS], NULL, thread, m);
+  }
+
+  for (int i = 0; i < MAXTHREADS + extra_thread; i++) {
     pthread_join(thread_id[i], NULL);
   }
 
@@ -76,7 +87,14 @@ void parallel_matrix_multiply(double **C, double **A, double **B, int a_rows, in
 
 void *thread(void *arg) {
   struct matrix_mult m = *((struct matrix_mult *) arg);
-  m.C[m.i][m.j] += m.A[m.i][m.k] * m.B[m.k][m.j];
+
+  for (int i = m.a_start_row; i < m.a_end_row; i++) {
+    for (int j = 0; j < m.b_cols; j++) {
+      m.C[i][j] = 0;
+      for (int k = 0; k < m.a_cols; k++)
+        m.C[i][j] += m.A[i][k] * m.B[k][j];
+    }
+  }
   return NULL;
 }
 
@@ -89,6 +107,26 @@ Parallel: 3.774s
 0.00x speedup between naive and parallel
 - Results don't match, presumably rounding issue
 
+- Dividing rows among threads: n = 500
+;; Threads = 2
+Naive: 0.579s
+Parallel: 0.293s
+1.98x speedup between naive and parallel
 
+;; Threads = 4
+Naive: 0.576s
+Parallel: 0.146s
+3.95x speedup between naive and parallel
 
+;; Threads = 8
+Naive: 0.574s
+Parallel: 0.086s
+6.71x speedup between naive and parallel
+
+;; Threads = 16
+Naive: 0.577s
+Parallel: 0.078s
+7.41x speedup between naive and parallel
+
+Did not indrease twofolds after this point
  */
