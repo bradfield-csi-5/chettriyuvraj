b8c7ba9f49722f487d024ed5802938edf6147fa2
chettriyuvraj@gmail.com <chettriyuvraj@gmail.com>
2023-11-06 20:41:01 +0530

    1. feat: single thread per computation

--- BradfieldCSI/Compilers/concurrency-1-prework/matrix/matrix-multiply.c
+++ BradfieldCSI/Compilers/concurrency-1-prework/matrix/matrix-multiply.c
@@ -2,9 +2,14 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-#define MAXTHREADS 100
+#define MAXTHREADS 100000
 void *thread(void *arg);
-
+struct matrix_mult {
+  double **A;
+  double **B;
+  double **C;
+  int i, j, k;
+};
 
 /*
   A naive implementation of matrix multiplication.
@@ -20,24 +25,70 @@ void matrix_multiply(double **C, double **A, double **B, int a_rows, int a_cols,
         C[i][j] += A[i][k] * B[k][j];
     }
   }
+
+  /* Print result */
+  // for (int i = 0; i < a_rows; i++) {
+  //   for (int j = 0; j < b_cols; j++)
+  //     printf("%f ", C[i][j]);
+  //   printf("\n");
+  // }
+
+
 }
 
-void parallel_matrix_multiply(double **c, double **a, double **b, int a_rows, int a_cols, int b_cols) {
+void parallel_matrix_multiply(double **C, double **A, double **B, int a_rows, int a_cols, int b_cols) {
   pthread_t thread_id[MAXTHREADS];
-  int thread_count = 5;
-  for (int i = 0; i < thread_count; i++) {
-    pthread_create(&thread_id[i], NULL,thread, NULL);
+  struct matrix_mult * m;
+  int thread_count = 0;
+
+  /* One thread for each computation */
+  for (int i = 0; i < a_rows; i++) {
+    for (int j = 0; j < b_cols; j++) {
+      C[i][j] = 0;
+      for (int k = 0; k < a_cols; k++) {
+        m = malloc(sizeof(struct matrix_mult));
+        // m =  {A, B, C, i, j, k};
+        (*m).A = A;
+        (*m).B = B;
+        (*m).C = C;
+        (*m).i = i;
+        (*m).j = j;
+        (*m).k = k;
+        pthread_create(&thread_id[thread_count++], NULL, thread, m);
+      }
+    }
   }
 
+  for (int i = 0; i<thread_count; i++) {
+    pthread_join(thread_id[i], NULL);
+  }
+
+  /* Print result */
+  // printf("\n\n Parallel Result\n");
+  // for (int i = 0; i < a_rows; i++) {
+  //   for (int j = 0; j < b_cols; j++) {
+  //     printf("%f ", C[i][j]);
+  //   }
+  //   printf("\n");
+  // }
+
 }
 
-void *thread (void *arg) {
-  printf("\nThread id %ld", pthread_self());
+void *thread(void *arg) {
+  struct matrix_mult m = *((struct matrix_mult *) arg);
+  m.C[m.i][m.j] += m.A[m.i][m.k] * m.B[m.k][m.j];
   return NULL;
 }
 
 
 /* 
-1. 
+- Single thread per computation:
+n = 30 
+Naive: 0.000s
+Parallel: 3.774s
+0.00x speedup between naive and parallel
+- Results don't match, presumably rounding issue
+
+
 
  */
